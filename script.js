6a258ae0f1d6590c6432c8b6df44036dded51cb0
Arpita Nath <arpita.nath@walmartlabs.com>
2020-10-08 16:53:26 +0530

    finding neighbors with BOMB

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const grid = document.querySelector(".grid");
-let rows = 10,
-  col = 10;
-let bombCount = 20;
-grid.style.width = `${rows * 50}px`;
-grid.style.height = `${col * 50}px`;
+let rows = 5;
+col = 3;
+let bombCount = (rows * col) / 2;
+grid.style.height = `${rows * 50}px`;
+grid.style.width = `${col * 50}px`;
 let fragment = document.createDocumentFragment();
 
 //data
 let data = [];
 let bombs = [];
 
-//generate bombs
+//generate random bombs
 for (let i = 0; i < bombCount; i++) {
   bombs.push([
-    Math.floor(Math.random() * Math.floor(10)),
-    Math.floor(Math.random() * Math.floor(10))
+    Math.floor(Math.random() * Math.floor(rows)),
+    Math.floor(Math.random() * Math.floor(col))
   ]);
 }
 
@@ -25,12 +25,9 @@ function createModel() {
     for (let j = 0; j < col; j++) {
       //populating our model
       data[i].push({
-        id: rows * i + j,
         row: i,
         col: j,
-        clicked: false,
-        bomb: false,
-        isAdjBombPresent: false
+        bomb: false
       });
     }
   }
@@ -40,31 +37,31 @@ function createModel() {
   });
 
   //get adjacent bombs count for each cell and update model
-  getNeighbors(data);
+  getNeighborsWithBomb(data);
 }
 
-function getNeighbors(data) {
+function getNeighborsWithBomb(data) {
   // possible combination of rows and columns
   let dx = [-1, -1, -1, 0, 0, 1, 1, 1];
   let dy = [-1, 0, 1, -1, 1, -1, 0, 1];
 
   // we are trying to find neighboring cells of item
   for (let i = 0; i < data.length; i++) {
-    console.log(data[i]);
     data[i].map(item => {
       item.neighbors = [];
       let itemRow = item.row;
       let itemCol = item.col;
       for (let j = 0; j < 8; j++) {
-        // console.log("positions", itemRow + dx[j], itemCol + dy[j]);
-        if (isValid(itemRow + dx[j], itemCol + dy[j])) {
+        if (
+          isValid(itemRow + dx[j], itemCol + dy[j]) &&
+          data[itemRow + dx[j]][itemCol + dy[j]].bomb
+        ) {
           item.neighbors.push([itemRow + dx[j], itemCol + dy[j]]);
         }
       }
     });
   }
-
-  console.log("data after modification", data);
+  console.log("data after finding neighbors", data);
 }
 
 // function to check whether row and column value are valid
@@ -79,20 +76,26 @@ function createUIFromModel(data) {
   data.map(item => {
     for (let j = 0; j < item.length; j++) {
       const square = document.createElement("div");
-      square.setAttribute("cell-data", JSON.stringify(item[j]));
-      square.setAttribute("id", item[0].row * rows + j);
+      square.setAttribute("row", item[j].row);
+      square.setAttribute("col", item[j].col);
       square.addEventListener("click", cellClick);
       fragment.appendChild(square);
     }
-    grid.appendChild(fragment);
   });
+  grid.appendChild(fragment);
 }
 
 function cellClick(e) {
-  let cellData = e.path[0].getAttribute("cell-data");
-  if (JSON.parse(cellData).bomb == true) {
-    console.log(e.target);
+  let ID = e.target.id;
+  let selectedRow = e.target.getAttribute("row");
+  let selectedCol = e.target.getAttribute("col");
+  console.log(selectedRow, selectedCol);
+  if (data[selectedRow][selectedCol].bomb) {
     e.target.className = "bomb";
+  } else {
+    console.log("selectedItem", data[selectedRow][selectedCol]);
+    let count = data[selectedRow][selectedCol].neighbors.length;
+    e.target.innerHTML = count;
   }
 }
 
